5b21f03fdffda142e88ca86b420b74045f591fc6
Ravindra Dhore <ravindrad@ansh.com>
2019-11-27 16:23:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { merge, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  defaultImage = '../assets/loader.gif';
+  image1 =
+    'https://images.unsplash.com/photo-1422004707501-e8dad229e17a?fm=jpg';
+  image2 =
+    'https://images.unsplash.com/photo-1439931444800-9bcc83f804a6?fm=jpg';
+  image3 =
+    'https://images.unsplash.com/photo-1417128281290-30a42da46277?fm=jpg';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LazyLoadImageModule, scrollPreset  } from 'ng-lazyload-image';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LazyLoadImageModule.forRoot({
+      preset: scrollPreset // <-- tell LazyLoadImage that you want to use scrollPreset
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
